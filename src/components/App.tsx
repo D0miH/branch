fc84b94ccf0b7bc65ea86f8cfe5278f9a7e81968
Dominik <dominik.hintersdorf@yahoo.de>
2019-03-01 18:22:55 +0100

    added toolbar and sidebar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import Titlebar from "./Titlebar";
+import Toolbar from "./Toolbar/Toolbar";
+import Sidebar from "./Sidebar/Sidebar";
 import "./App.css";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="AppContent" />
                 <Titlebar />
+                <div className="AppContent">
+                    <Toolbar />
+                    <div className="MainView">
+                        <Sidebar />
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Toolbar.css";
+
+class Toolbar extends React.Component {
+    render() {
+        return <div className="Toolbar" />;
+    }
+}
+
+export default Toolbar;

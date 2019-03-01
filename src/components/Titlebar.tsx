bf95136aafbf43e7e45d1d87136e0c81042b1180
Dominik <dominik.hintersdorf@yahoo.de>
2019-03-01 17:58:59 +0100

    added titlebar text

---
+++ src/components/Titlebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Titlebar.css";
+
+function Titlebar() {
+    return (
+        <div className="Titlebar">
+            <span>Branch</span>
+        </div>
+    );
+}
+
+export default Titlebar;

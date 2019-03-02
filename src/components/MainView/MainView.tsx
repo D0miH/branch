19ca3c7e060a857ea284e2c206aef8e668ad3366
Dominik <36657497+D0miH@users.noreply.github.com>
2019-03-02 17:59:44 +0100

    added basic layout with toolbar, sidebar and main view

---
+++ src/components/MainView/MainView.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MainView.css";
+
+class MainView extends React.Component {
+    render() {
+        return <div className="MainView" />;
+    }
+}
+
+export default MainView;

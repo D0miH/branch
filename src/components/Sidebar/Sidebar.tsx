14391c6b3f339fa83f1340380c643e4c325f4490
Dominik <dominik.hintersdorf@yahoo.de>
2019-03-02 19:08:45 +0100

    Added filter bar to sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import FilterBar from "./FilterBar";
 import "./Sidebar.css";
 
 class Sidebar extends React.Component {
     render() {
-        return <div className="Sidebar" />;
+        return (
+            <div className="Sidebar">
+                <FilterBar />
+            </div>
+        );
     }
 }
 

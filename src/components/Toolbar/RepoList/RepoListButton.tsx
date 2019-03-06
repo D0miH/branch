679679a9550a28110c76bcf3c8a9c794944610c5
Dominik <36657497+D0miH@users.noreply.github.com>
2019-03-06 15:22:11 +0100

    added overlay to repo list button

--- src/components/Toolbar/RepoList/RepoListButton.tsx
+++ src/components/Toolbar/RepoList/RepoListButton.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 import "./RepoListButton.css";
+import Overlay from "./Overlay";
 
 type Props = {
     repoName: String;
 };
 
-class RepoListButton extends React.Component<Props> {
+type State = {
+    listIsCollapsed: boolean;
+};
+
+class RepoListButton extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = { listIsCollapsed: true };
+    }
+
     openRepoList() {
+        this.setState({ listIsCollapsed: !this.state.listIsCollapsed });
         console.log("Repo list button was clicked");
     }
 
     render() {
         return (
-            <div className="RepoListButton" onClick={() => this.openRepoList()}>
+            <div
+                className="RepoListButton"
+                style={{ zIndex: this.state.listIsCollapsed ? 1 : 3 }}
+                onClick={() => this.openRepoList()}
+            >
                 <span>{this.props.repoName}</span>
+                <Overlay overlayVisible={!this.state.listIsCollapsed} />
             </div>
         );
     }

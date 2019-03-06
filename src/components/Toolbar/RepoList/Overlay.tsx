64a16455e1ca7dd73b2653c11e5c1dfcbce16ba0
Dominik <dominik.hintersdorf@yahoo.de>
2019-03-06 15:22:11 +0100

    added overlay to repo list button

---
+++ src/components/Toolbar/RepoList/Overlay.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type Props = {
+    overlayVisible: boolean;
+};
+
+class Overlay extends React.Component<Props> {
+    render() {
+        return this.props.overlayVisible ? (
+            <div
+                className="Overlay"
+                style={{
+                    position: "fixed",
+                    backgroundColor: "black",
+                    opacity: 0.5,
+                    width: "100%",
+                    height: "100%",
+                    zIndex: -1,
+                    top: "0px",
+                    left: "0px"
+                }}
+            />
+        ) : null;
+    }
+}
+
+export default Overlay;

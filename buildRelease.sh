e476ea33b0c9a0cebfa88c1c0a775c8a1c1b8953
Dominik <36657497+D0miH@users.noreply.github.com>
2019-03-14 17:34:30 +0100

    added build script

---
+++ buildRelease.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ "$TARGET_OS" == "linux" ]; then
+    yarn build && yarn build-electron && yarn run electron-builder -l
+elif [ "$TARGET_OS" == "osx" ]; then
+    yarn build && yarn build-electron && yarn run electron-builder -m
+elif [ "$TARGET_OS" == "osx" ]; then
+    yarn build && yarn build-electron && yarn run electron-builder -w
+fi

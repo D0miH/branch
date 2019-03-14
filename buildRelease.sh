2cd442618775883d13876ad18640302d5a641d5f
Dominik <36657497+D0miH@users.noreply.github.com>
2019-03-14 17:41:46 +0100

    added win, and linux

--- buildRelease.sh
+++ buildRelease.sh
@@ -4,6 +4,6 @@ if [ "$TARGET_OS" == "linux" ]; then
     yarn build && yarn build-electron && yarn run electron-builder -l
 elif [ "$TARGET_OS" == "osx" ]; then
     yarn build && yarn build-electron && yarn run electron-builder -m
-elif [ "$TARGET_OS" == "osx" ]; then
+elif [ "$TARGET_OS" == "win" ]; then
     yarn build && yarn build-electron && yarn run electron-builder -w
 fi

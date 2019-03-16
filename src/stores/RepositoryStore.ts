a1a731e71791c60d2b4867b01c84e2e39d289f20
Dominik <dominik.hintersdorf@yahoo.de>
2019-03-16 18:48:47 +0100

    local and remote branches can be fetched

--- src/stores/RepositoryStore.ts
+++ src/stores/RepositoryStore.ts
@@ -1,5 +1,39 @@
-import { observable } from "mobx";
+import { observable, action } from "mobx";
+import { getPackedSettings } from "http2";
 
 export default class RepositoryStore {
     @observable currentRepoName: string = "";
+
+    @observable localBranches: string[] = [];
+    @observable remotes: string[] = [];
+    @observable tags: string[] = [];
+
+    @action openRepo(repoPath: string) {
+        // open the repo
+        let repoName = window.ipcRenderer.sendSync("open-repo", repoPath);
+        this.currentRepoName = repoName;
+
+        // get all the local branches
+        this.getLocalBranches();
+        // get all the remote branches
+        this.getRemoteBranches();
+    }
+
+    @action getLocalBranches() {
+        // get all the local branches
+        let branches: string[] = window.ipcRenderer.sendSync(
+            "get-local-branches"
+        );
+        console.log(branches);
+    }
+
+    @action getRemoteBranches() {
+        // get all the remote branches
+        let branches: string[] = window.ipcRenderer.sendSync(
+            "get-remote-branches"
+        );
+        console.log(branches);
+    }
+
+    @action getTags() {}
 }

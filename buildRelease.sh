2056f3842c8f4560dc770ca976f439de469848e8
Dominik <36657497+D0miH@users.noreply.github.com>
2019-03-19 10:05:12 +0100

    added renaming of linux deb file

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TARGET_OS" == "linux" ]; then
-    yarn build && yarn build-electron && yarn run electron-builder -l
+    yarn build && yarn build-electron && yarn run electron-builder -l && rename -f "s/branch/Branch/" ./dist/branch_*.deb
 elif [ "$TARGET_OS" == "osx" ]; then
     yarn build && yarn build-electron && yarn run electron-builder -m
 elif [ "$TARGET_OS" == "win" ]; then

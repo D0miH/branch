f6d1cc59f8ca38e3b9729ab62b6da25e5d1baa16
Dominik <36657497+D0miH@users.noreply.github.com>
2019-04-07 10:38:38 +0200

    promisify-electron-ipc is now used to pull

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -1,7 +1,9 @@
 import { IpcRenderer } from "electron";
+import { PromisifiedIpcRenderer } from "promisify-electron-ipc/lib/renderer";
 
 declare global {
     interface Window {
         ipcRenderer: IpcRenderer;
+        promiseIpcRenderer: PromisifiedIpcRenderer;
     }
 }

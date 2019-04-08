db78739ac255eba49d481722665d06bae354c4b5
Dominik <dominik.hintersdorf@yahoo.de>
2019-04-08 18:09:35 +0200

    added husky hooks

--- src/stores/CommitSidebarStore.ts
+++ src/stores/CommitSidebarStore.ts
@@ -3,10 +3,10 @@ import { IGitCommit } from "../typings/git-types";
 
 export default class CommitSidebarStore {
     @observable selectedCommit: IGitCommit = {
+        author: "",
         commitDate: "",
         commitMessage: "",
         commitTime: "",
-        author: "",
         hash: ""
     };
 }
